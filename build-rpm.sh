93980290d93e9b547b413eff9007e699184fa386
John Ford <jhford@users.noreply.github.com>
2017-04-06 16:37:52 +0200

    Build rpm improvements (#14)
    * Updates of the RPM packaging section
    
    * Don't run the brp-* family of post install scripts for rpm building
    
    * bump to v1.0.4

--- build-rpm.sh
+++ build-rpm.sh
@@ -14,7 +14,11 @@ sed -e "s/__VERSION__/$VERSION/" < host-secrets.spec.in > host-secrets.spec
 tar -zc -f $TOPDIR/SOURCES/taskcluster-host-secrets.tar.gz $(git ls-files)
 spectool -C $TOPDIR/SOURCES -g host-secrets.spec
 rpmbuild --define "version $VERSION" --define "_topdir $TOPDIR" -bs host-secrets.spec
+rm -rf results/
+mkdir results/
 #for env in epel-6-x86_64 ; do
 for env in fedora-25-x86_64 ; do
-  mock --define "version $VERSION" -r $env $TOPDIR/SRPMS/taskcluster-host-secrets*.src.rpm
+  rm -rf results/$env
+  mkdir results/$env
+  mock --define "version $VERSION" -r $env $TOPDIR/SRPMS/taskcluster-host-secrets*.src.rpm --resultdir results/$env
 done
